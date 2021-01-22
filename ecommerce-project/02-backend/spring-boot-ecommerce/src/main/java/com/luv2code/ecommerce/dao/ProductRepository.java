672af44bcc341e3b57122b1f9b590c730400ff34
geopasx2020 <gkaramousalidis@hotmail.gr>
2021-01-22 13:43:18 +0200

    Commit 22/1/2021

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
+
 @CrossOrigin("http://localhost:4200")
-public interface ProductRepository extends JpaRepository<Product, Long> { //path /products
+public interface ProductRepository extends JpaRepository<Product, Integer> { //path /products
+    List<Product> findByDescription(String description);
 
 }
 
--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -1,5 +1,6 @@
 package com.luv2code.ecommerce.entity;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +9,9 @@ import java.util.Set;
 
 @Entity
 @Table(name="product_category")
-// @Data -- known bug
-@Getter
-@Setter
+//@Data
+//@Getter
+//@Setter
 public class ProductCategory {
 
     @Id
@@ -21,8 +22,40 @@ public class ProductCategory {
     @Column(name = "category_name")
     private String categoryName;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+   @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private Set<Product> products;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "id=" + id +
+                ", categoryName='" + categoryName + '\'' +
+                ", products=" + products +
+                '}';
+    }
 }
 
